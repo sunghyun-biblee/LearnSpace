5ac40d230d7d2b4231c9794835a581c1e2fb95cc
sunghyun1014 <sunghyun1014@naver.com>
2023-11-14 21:22:32 +0900

    객체 생성을 위해 필드를 작성하고 기본생성자,매개변수생성자를 작성, 결과확인을 위해 toString 작성

---
+++ 6_FrameWork/Spring03/src/com/test04/Developer.java
@@ -0,0 +1,38 @@
+package com.test04;
+
+public class Developer {
+	private Emp emp;
+	private String dept;
+	
+	public Developer() {}
+	
+	public Developer(Emp emp, String dept) {
+		this.emp=emp;
+		this.dept=dept;
+	}
+
+	public Emp getEmp() {
+		return emp;
+	}
+
+	public void setEmp(Emp emp) {
+		this.emp = emp;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	@Override
+	public String toString() {
+		return emp + "\t 부서: " + dept ;
+	}
+	
+	
+	
+	
+}
