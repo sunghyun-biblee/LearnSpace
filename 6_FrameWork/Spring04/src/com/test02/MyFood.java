9e21593af238c0ee3334a8a0b0a62b53de161311
sunghyun1014 <sunghyun1014@naver.com>
2023-11-14 21:25:35 +0900

    객체 생성을 위해 필드를 작성하고, 기본생성자, 매개변수 생성자를 작성, 결과확인을 위해 toString 작성

---
+++ 6_FrameWork/Spring04/src/com/test02/MyFood.java
@@ -0,0 +1,39 @@
+package com.test02;
+
+public class MyFood {
+	private Food favoriteFood;
+	private Food unFavoriteFood;
+	
+	public MyFood() {
+		super();
+	}
+	
+	public MyFood(Food favoriteFood, Food unFavoriteFood) {
+		super();
+		this.favoriteFood = favoriteFood;
+		this.unFavoriteFood = unFavoriteFood;
+	}
+
+	public Food getFavoriteFood() {
+		return favoriteFood;
+	}
+
+	public void setFavoriteFood(Food favoriteFood) {
+		this.favoriteFood = favoriteFood;
+	}
+
+	public Food getUnFavoriteFood() {
+		return unFavoriteFood;
+	}
+
+	public void setUnFavoriteFood(Food unFavoriteFood) {
+		this.unFavoriteFood = unFavoriteFood;
+	}
+
+	@Override
+	public String toString() {
+		return "favoriteFood= " + favoriteFood +"\nunFavoriteFood= " + unFavoriteFood ;
+	}
+	
+	
+}
