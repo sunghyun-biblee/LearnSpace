e484b7d2b04600a86f85115ea6d924e54c9aab58
sunghyun1014 <sunghyun1014@naver.com>
2023-11-16 17:42:49 +0900

    객체를 생성하고 잘 동작하는지 확인하기위한 클래스

---
+++ 6_FrameWork/Spring06_AOP/src/com/test05/MTest.java
@@ -0,0 +1,22 @@
+package com.test05;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MTest {
+
+	public static void main(String[] args) {
+		ApplicationContext context= new ClassPathXmlApplicationContext("com/test05/applicationContext.xml");
+		
+		Person w= context.getBean("woman",Person.class);
+		Person m= context.getBean("man",Person.class);
+		
+		System.out.println("여학생 기상");
+		w.classWork();
+		
+		System.out.println("----------");
+		System.out.println("남학생 기상");
+		m.classWork();
+	}
+
+}
