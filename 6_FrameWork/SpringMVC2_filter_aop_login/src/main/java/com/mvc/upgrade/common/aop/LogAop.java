589c773adaa544a5771d3884f666b2c2bfd8551a
sunghyun1014 <sunghyun1014@naver.com>
2023-11-20 21:49:57 +0900

    포인트컷을 기준으로 포인트컷 메소드를 실행하기 전 , 후 , 예외발생 하였을때 실행할 메소드들을 작성

---
+++ 6_FrameWork/SpringMVC2_filter_aop_login/src/main/java/com/mvc/upgrade/common/aop/LogAop.java
@@ -0,0 +1,31 @@
+package com.mvc.upgrade.common.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogAop {
+
+	public void before(JoinPoint join) {
+		Logger logger = LoggerFactory.getLogger(join.getTarget()+"");
+		logger.info("-----Aop Start-----");
+		
+		Object[] args = join.getArgs();	//타켓 실행시 파라미터
+		if(args != null) {
+			logger.info("method: "+join.getSignature().getName());//타켓메소드정보
+			for(int i=0; i<args.length; i++) {
+				logger.info(i+"번째: " + args[i]);
+			}
+		}
+	}
+	public void after(JoinPoint join) {
+		Logger logger = LoggerFactory.getLogger(join.getTarget()+"");
+		logger.info("-----Aop End-----");
+	}	
+	public void afterThrowing(JoinPoint join) {
+		Logger logger = LoggerFactory.getLogger(join.getTarget()+"");
+		logger.info("-----Error Log-----");
+		logger.info("ERROR: " + join.getArgs());
+		logger.info("ERROR: " + join.toString());
+	}
+}
