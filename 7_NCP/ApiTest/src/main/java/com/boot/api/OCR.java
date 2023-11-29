022f4a1230f8abde771c23c6c0088009ebba3e93
sunghyun1014 <sunghyun1014@naver.com>
2023-11-29 21:59:50 +0900

    문서에서 해당부분을 필드로 설정하고 api를 사용하여 해당부분을 번역해주는 api를 활용하여봄

---
+++ 7_NCP/ApiTest/src/main/java/com/boot/api/OCR.java
@@ -0,0 +1,116 @@
+package com.boot.api; // api 복사붙여넣기 할때 패키지가 사라질 수 있으니 확인
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+import org.json.JSONArray; //org.json 검색하여 추가해도 무관
+import org.json.JSONObject;
+
+public class OCR {
+
+	public String ocrTest(String path) {
+		String apiURL = "https://sq697edw3s.apigw.ntruss.com/custom/v1/26554/9d6395756e3aeed5f257ce96ab1a1a1bd8d67c021f97b16ba0c1de17f4c6f235/infer"; // 해당부분에 너의 api url을 넣어라
+		String secretKey = "cFdXZURFcVdzVEJ0ZEJFS0RKclRVTEpKaWZZU0tWY0M="; // 해당부분에 너의 시크릿 키 을 넣어라
+		String imageFile = path+"\\img01.jpg"; // 해당부분에 이미파일을 넣어라
+		StringBuffer response=null;
+
+		
+		
+		// apicontroller에 돌려줘야하니 Stringbuffer를 이용하여 리턴
+		// 리턴을 주기위해 stringbuffer를 지역변수로 올림
+		try {
+			URL url = new URL(apiURL);
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			con.setUseCaches(false);
+			con.setDoInput(true);
+			con.setDoOutput(true);
+			con.setReadTimeout(30000);
+			con.setRequestMethod("POST");
+			String boundary = "----" + UUID.randomUUID().toString().replaceAll("-", "");
+			con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+			con.setRequestProperty("X-OCR-SECRET", secretKey);
+
+			JSONObject json = new JSONObject();
+			json.put("version", "V2");
+			json.put("requestId", UUID.randomUUID().toString());
+			json.put("timestamp", System.currentTimeMillis());
+			JSONObject image = new JSONObject();
+			image.put("format", "jpg");
+			image.put("name", "demo");
+			JSONArray images = new JSONArray();
+			images.put(image);
+			json.put("images", images);
+			String postParams = json.toString();
+
+			con.connect();
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			long start = System.currentTimeMillis();
+			File file = new File(imageFile);
+			writeMultiPart(wr, postParams, file, boundary);
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			if (responseCode == 200) {
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			String inputLine;
+			response = new StringBuffer();  // 결과값 을 받아
+			while ((inputLine = br.readLine()) != null) {
+				response.append(inputLine);
+			}
+			br.close();
+
+			System.out.println(response);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return response.toString();
+	}
+
+	private static void writeMultiPart(OutputStream out, String jsonMessage, File file, String boundary) throws
+		IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("--").append(boundary).append("\r\n");
+		sb.append("Content-Disposition:form-data; name=\"message\"\r\n\r\n");
+		sb.append(jsonMessage);
+		sb.append("\r\n");
+
+		out.write(sb.toString().getBytes("UTF-8"));
+		out.flush();
+
+		if (file != null && file.isFile()) {
+			out.write(("--" + boundary + "\r\n").getBytes("UTF-8"));
+			StringBuilder fileString = new StringBuilder();
+			fileString
+				.append("Content-Disposition:form-data; name=\"file\"; filename=");
+			fileString.append("\"" + file.getName() + "\"\r\n");
+			fileString.append("Content-Type: application/octet-stream\r\n\r\n");
+			out.write(fileString.toString().getBytes("UTF-8"));
+			out.flush();
+
+			try (FileInputStream fis = new FileInputStream(file)) {
+				byte[] buffer = new byte[8192];
+				int count;
+				while ((count = fis.read(buffer)) != -1) {
+					out.write(buffer, 0, count);
+				}
+				out.write("\r\n".getBytes());
+			}
+
+			out.write(("--" + boundary + "--\r\n").getBytes("UTF-8"));
+		}
+		out.flush();
+	}
+}
